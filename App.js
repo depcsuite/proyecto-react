1a366e5ddf820e6c46ad2d6dc79ec8de95ec5ae9
israShort <israshort@gmail.com>
2021-10-13 10:06:38 -0300

    First commit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from './src/navigation/DrawerNavigator';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { AuthProvider } from './src/navigation/AuthProvider';
+import { AuthProvider }  from './src/navigation/AuthProvider';
 
 export default function App() {
 
