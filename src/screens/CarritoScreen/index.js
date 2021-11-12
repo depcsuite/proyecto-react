da2fa411ca37535d603e268ac0fd6a709638aaf6
israShort <israshort@gmail.com>
2021-11-12 18:50:09 -0300

    Event focus

--- src/screens/CarritoScreen/index.js
+++ src/screens/CarritoScreen/index.js
@@ -1,26 +1,23 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Text, View, FlatList, Dimensions, Button, RefreshControl } from 'react-native';
+import { Text, View, FlatList, Dimensions, Button, RefreshControl, ActivityIndicator } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import { AuthContext } from '../../navigation/AuthProvider';
 
-export default function CarritoScreen() {
+export default function CarritoScreen({ navigation }) {
 
     const { data, actions } = useContext(AuthContext);
 
     const [total, setTotal] = useState(0);
 
-    const [loading, setLoading] = useState(false);
-    const [refresh, setRefresh] = useState(true);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setLoading(true);
-        let cant = 0;
-        data.carrito.forEach(function (item) {
-            cant += item.total;
+        const unsubscribe = navigation.addListener('focus', () => {
+            retrieve();
         });
-        setTotal(cant);
-        setLoading(false);
-    }, [data.carrito, refresh]);
+
+        return unsubscribe;
+    }, []);
 
     const renderItem = ({ item }) => {
         return (
@@ -41,40 +38,58 @@ export default function CarritoScreen() {
         );
     }
 
-    const handleRefresh = () => {
-        setRefresh(prevValue => !prevValue);
+    const retrieve = () => {
+        setLoading(true);
+        let cant = 0;
+        data.carrito.forEach(function (item) {
+            cant += item.total;
+        });
+        setTotal(cant);
+        setLoading(false);
     }
 
-    return (
-        <FlatList
-            data={data.carrito}
-            renderItem={renderItem}
-            keyExtractor={item => `${item.id}`}
-            horizontal={false}
-            ListEmptyComponent={
-                <View style={{ alignItems: 'center' }}>
-                    <Text>No hay productos agregados al carrito.</Text>
-                </View>
-            }
-            ListFooterComponent={
-                data.carrito.length == 0 ?
-                    <></>
-                    :
-                    <View style={{ width: Dimensions.get('screen').width, alignItems: 'center' }}>
-                        <Text style={{ marginVertical: 20, fontSize: 17 }}>Total {total}</Text>
-                        <Button
-                            title={'Registrar pedido'}
-                            onPress={() => {
-                                actions.registrarPedido();
-                            }}
-                        />
+    const handleRefresh = () => {
+        retrieve();
+    }
+
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center' }}>
+                <ActivityIndicator size={'large'} color={'#000'} />
+            </View>
+        );
+    } else {
+        return (
+            <FlatList
+                data={data.carrito}
+                renderItem={renderItem}
+                keyExtractor={item => `${item.id}`}
+                horizontal={false}
+                ListEmptyComponent={
+                    <View style={{ alignItems: 'center' }}>
+                        <Text>No hay productos agregados al carrito.</Text>
                     </View>
-            }
-            style={{
-                backgroundColor: 'white',
-                paddingTop: 20
-            }}
-            refreshControl={<RefreshControl onRefresh={handleRefresh} refreshing={loading} />}
-        />
-    );
+                }
+                ListFooterComponent={
+                    data.carrito.length == 0 ?
+                        <></>
+                        :
+                        <View style={{ width: Dimensions.get('screen').width, alignItems: 'center' }}>
+                            <Text style={{ marginVertical: 20, fontSize: 17 }}>Total {total}</Text>
+                            <Button
+                                title={'Registrar pedido'}
+                                onPress={() => {
+                                    actions.registrarPedido();
+                                }}
+                            />
+                        </View>
+                }
+                style={{
+                    backgroundColor: 'white',
+                    paddingTop: 20
+                }}
+                refreshControl={<RefreshControl onRefresh={handleRefresh} refreshing={loading} />}
+            />
+        );
+    }
 }
